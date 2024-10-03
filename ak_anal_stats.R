10c5b5d15dc6df8f2f01dfcdf3c54e9024c63a9b
BC0LL1N5 <brennencollins44@gmail.com>
2024-10-02 20:45:36 -0500

    updated r script to be able to include overall stats for AK (avg,tot, tot_att,media, success_rate)

--- ak_anal_stats.R
+++ ak_anal_stats.R
@@ -11,7 +11,23 @@ ak_anal <- data %>%
          yards_gained, ydstogo, yrdln) %>%
   mutate(
     down = factor(down),
-    quater = factor(qtr)
+    quater = factor(qtr),
+    success = case_when(
+      down == 1 ~ yards_gained >= 0.4 * ydstogo,
+      down == 2 ~ yards_gained >= 0.6 * ydstogo,
+      down %in% c(3, 4) ~ yards_gained >= ydstogo,
+      TRUE ~ FALSE
+    )
   )
 view(ak_anal)
 
+
+overall_stats <- ak_anal %>%
+  summarise(
+    total_attempts = n(),
+    total_yards = sum(yards_gained),
+    avg_yards_per_attempt = mean(yards_gained),
+    median_yards = median(yards_gained),
+    success_rate = mean(success, na.rm = TRUE)
+  )
+View(overall_stats)
